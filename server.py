08c8496d7057685c4c38a964d8de959c477cdc39
Sorseg <samoylovfp@gmail.com>
2013-04-05 10:16:44 +0400

    Configured logging and enabled static handler

--- server.py
+++ server.py
@@ -4,19 +4,27 @@ import tornado.ioloop
 import tornado.web
 import tornado.websocket
 import logic
+import commands
+import logging
+import uuid
+
+logging.basicConfig(filename='caster.log', level=logging.DEBUG)
 
 class MainHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        print("WebSocket opened")
+        pass
 
     def on_message(self, message):
-        logic.digest(self, message)
+        commands.do(self, message)
 
     def on_close(self):
-        print ("WebSocket closed")
+        #TODO: logout
+        pass
+    
 
 application = tornado.web.Application([
     (r"/", MainHandler),
+    (r"/static/(.*)", tornado.web.StaticFileHandler, {'path':'./static'}),
 ],
     debug=True)
 
