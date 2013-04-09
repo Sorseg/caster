69c8e52d6d9935e3b9bdc63ef7807c252ffc4cee
Sorseg <samoylovfp@gmail.com>
2013-04-10 00:18:12 +0400

    started implementation of updaters

--- logic.py
+++ logic.py
@@ -1,14 +1,20 @@
+from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor
+from functools import wraps
 from threading import RLock
 import database as db
-from functools import wraps
-from collections import defaultdict
+import tornado.ioloop
+import logging
 
 EXECUTOR = ThreadPoolExecutor(max_workers=4)
+TIMEOUT = 15
 
 loc_locks = defaultdict(RLock)
+loc_updaters = {}
+loc_requests = defaultdict(list)
 
-def locking(loc_id):
+
+def locking(loc_id, can_cancel = True):
     def wrapper(func):
         @EXECUTOR.submit
         def job():
@@ -19,3 +25,24 @@ def locking(loc_id):
 
 def send_environment(player):
     pass
+
+def check_update(player):
+    '''TODO: Add updater as soon as player enters location
+    '''
+    
+def update_loc(id):
+    def updater():
+        #TODO:
+        logging.info("updating", id)
+    return updater
+    
+    
+def init(loop):
+    with db.Handler() as h:
+        for location in h.session.query(db.Location):
+            if location.creatures:
+                upd = tornado.ioloop.PeriodicCallback(update_loc(location.id), 1000*TIMEOUT, loop)
+                loc_updaters[location.id] = upd
+                upd.start()
+
+                
