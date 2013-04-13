4131b6cbff2d3c20611a5f2081f8d63ffe1c20a4
Sorseg <samoylovfp@gmail.com>
2013-04-14 02:17:19 +0400

    Added socket server

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ import tornado.websocket
 
 import commands
 import logic
-
+from tornado.tcpserver import TCPServer
 logging.basicConfig(filename='caster.log', level=logging.DEBUG)
 
 class Player:
@@ -38,7 +38,31 @@ class MainHandler(tornado.websocket.WebSocketHandler):
 
     def on_close(self):
         Player.players.pop(self.player.login, None)
+
+
+class SocketServer(TCPServer):
     
+    def handle_stream(self, stream, addr):
+        logging.info("Connection from {!r}".format(addr))
+        stream.player = Player(stream)
+        
+        def write_message(msg):
+            stream.write(bytes(json.dumps(msg),'utf8')+b'\n')    
+        stream.write_message = write_message
+        
+        def on_close():
+            Player.players.pop(stream.player.login, None)
+        stream.set_close_callback(on_close)
+        
+        def on_message( msg = None):
+            if msg != None:
+                logging.debug("Sockets message: {!s}".format(msg))
+                commands.do(stream, msg.decode('utf8'))
+            stream.read_until(b'\n', on_message)
+            
+        on_message()
+    
+        
 
 application = tornado.web.Application([
     (r"/", MainHandler),
@@ -50,4 +74,7 @@ if __name__ == "__main__":
     application.listen(8888)
     loop = tornado.ioloop.IOLoop.instance()
     logic.init(loop)
+    server = SocketServer()
+    server.bind(8889)
+    server.start()
     loop.start()
